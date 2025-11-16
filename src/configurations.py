73e26291119115cd1f4e7a5db60d8499ce82d5b2
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-16 22:33:35 +0200

    update the configurations module file

--- src/configurations.py
+++ src/configurations.py
@@ -1,1 +1,35 @@
 
+## this is module file for reloading certain functions for reusability:
+
+def X_train_y_train(path="data/train_data.csv"):
+    import pandas as pd
+
+    train_df=pd.read_csv(path)
+    X_train=train_df.drop(["feedback_text","country","last_purchase_date","customer_id","gender"],axis=1)
+    y_train=train_df["churned"]
+
+    return X_train,y_train
+
+##create a function for getting the model's metrics:
+
+def gilbert_skill_score(y_true,y_pred):
+    from sklearn.metrics import confusion_matrix as conf_mat
+
+    tp,fp,fn,tn=conf_mat(y_true,y_pred).ravel()
+    e=tp*(fp+fn)/len(y_true)
+
+    gss=(tp-e)/(tp+fp+fn-e)
+
+    return gss
+
+
+def model_metrics(y_true,X_test,model):
+    from sklearn.metrics import accuracy_score,cohen_kappa_score,matthews_corrcoef
+    y_pred=model.predict(X_test)
+    acc_score=accuracy_score(y_true,y_pred)
+    kappa_score=cohen_kappa_score(y_true,y_pred)
+    mat_score=matthews_corrcoef(y_true,y_pred)
+    gilbert_score=gilbert_skill_score(y_true,y_pred)
+
+    return {"accuracy":acc_score,"kappa_score":kappa_score,"mat_corr":mat_score,"gilbert_score":gilbert_score}
+  
