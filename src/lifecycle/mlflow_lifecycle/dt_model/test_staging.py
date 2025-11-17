be994de4409936f55e1cd24bafba64c420ec6a26
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-17 07:57:06 +0200

    added code for testing model staging

--- src/lifecycle/mlflow_lifecycle/dt_model/test_staging.py
+++ src/lifecycle/mlflow_lifecycle/dt_model/test_staging.py
@@ -1,1 +1,17 @@
+from configurations_mlflow import set_mlflow_exp,set_mlflow_host,load_dt_model_name
+from configurations_mlflow import test_model_versioning,mlflow_client
 
+
+## set the mlflow experiment and mlflow host within the workflow:
+set_mlflow_exp()
+set_mlflow_host()
+
+model_name=load_dt_model_name()
+model_stage="staging"
+
+## client:
+client=mlflow_client()
+
+test_stage_phase=test_model_versioning(name=model_name,stage=model_stage,client_var=client)
+
+print(test_stage_phase)
