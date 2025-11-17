d726d6bb316759981774e44f11f57269b1c70040
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-17 07:45:45 +0200

    log model staging

---
+++ src/lifecycle/mlflow_lifecycle/log_model/staging.py
@@ -0,0 +1,23 @@
+import mlflow.sklearn
+from configurations_mlflow import set_mlflow_exp,set_mlflow_host,mlflow_client,load_log_model_name
+
+
+## set the mlflow host and mlflow experience within the workflow first:
+set_mlflow_host()
+set_mlflow_exp()
+
+## get the model's parameters:
+
+model_name=load_log_model_name()
+model_version="1"
+
+## get the client:
+client=mlflow_client()
+
+
+## stage the model in mlflow:
+client.transition_model_version_stage(
+    name=model_name,
+    version=model_version,
+    stage="staging"
+)
