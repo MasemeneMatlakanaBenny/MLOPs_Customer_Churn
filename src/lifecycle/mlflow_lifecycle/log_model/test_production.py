a021345bb845ea3d56a78657f9e58680aff218ba
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-17 07:47:39 +0200

    testing the model's production phase

--- src/lifecycle/mlflow_lifecycle/log_model/test_production.py
+++ src/lifecycle/mlflow_lifecycle/log_model/test_production.py
@@ -1,1 +1,18 @@
 
+from configurations_mlflow import set_mlflow_exp,set_mlflow_host,load_log_model_name
+from configurations_mlflow import test_model_versioning,mlflow_client
+
+
+## set the mlflow experiment and mlflow host within the workflow:
+set_mlflow_exp()
+set_mlflow_host()
+
+model_name=load_log_model_name()
+model_stage="production"
+
+## client:
+client=mlflow_client()
+
+test_prod_phase=test_model_versioning(name=model_name,stage=model_stage,client_var=client)
+
+print(test_prod_phase)
