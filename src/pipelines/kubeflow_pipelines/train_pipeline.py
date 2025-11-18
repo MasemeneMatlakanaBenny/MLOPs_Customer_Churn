e3d9a21abb64c43e90bf7ac4a7467764fe1eaaaa
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-18 21:54:47 +0200

    add the data loading component to the kf[p train pipeline

--- src/pipelines/kubeflow_pipelines/train_pipeline.py
+++ src/pipelines/kubeflow_pipelines/train_pipeline.py
@@ -1,1 +1,14 @@
+from kfp import dsl,compiler
+from kfp.dsl import pipeline,component
+
+@component
+def data_loading_component():
+  """
+  Component for loading the X_train and y_train
+  """
+  from configurations import X_train_y_train
+  
+  X_train,y_train=X_train_y_train()
+  
+  return X_train,y_train
 
