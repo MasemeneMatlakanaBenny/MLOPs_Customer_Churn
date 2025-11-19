4903d5b4634f5046b668e3803f6bc8fa6c0f7f50
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-19 11:33:00 +0200

    Refactor train_flow to use Prefect tasks

--- src/pipelines/prefect_pipelines/train_pipeline.py
+++ src/pipelines/prefect_pipelines/train_pipeline.py
@@ -36,13 +36,13 @@ def green_model_training(X_train,y_train):
   
   return model
 
-@flow
+@flow(name="Model Development Task Runner",task_runner=ConcurrentTaskRunner())
 def train_flow():
   import joblib
   
-  X_train,y_train=data_loading()
-  blue_model=blue_model_training(X_train=X_train,y_train=y_train)
-  green_model=green_model_trainig(X_train=X_train,y_train=y_train)
+  X_train,y_train=data_loading.submit()
+  blue_model=blue_model_training.submit(X_train=X_train,y_train=y_train)
+  green_model=green_model_trainig.submit(X_train=X_train,y_train=y_train)
   joblib.dump(blue_model,"models/blue_model.pkl")
   joblib.dump(green_model,"models/green_model.pkl")
   
