6888369b9d5253359d3209eabf5557a06bad3e69
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-19 21:33:39 +0200

    Implement model evaluation and metrics saving tasks
    Added tasks for loading models, evaluating performance, and saving metrics.

--- src/pipelines/prefect_pipelines/evaluation.py
+++ src/pipelines/prefect_pipelines/evaluation.py
@@ -1,2 +1,48 @@
+import pandas as pd
+import joblib
+from configurations import X_train_y_train,model_metrics
 from prefect import task,flow
+from prefect.task_runners import ConcurrentTaskRunner,DistributedTaskRunner
+from datetime import timedelta
 
+## load the dataset to use for evaluation:
+@task
+def load_data():
+  X_test,y_test=X_train_y_train(path="data/test_data.csv")
+  
+  return X_test,y_test
+
+## load the models using joblib
+@task(cache_fn=task.cache_fn,
+      cache_expiration=timedelta(days=100))
+def load_log_model():
+  log_model=joblib.load("models/blue_model.pkl")
+  
+  return log_model
+
+@task(cache_fn=task.cache_fn,
+      cache_expiration=timedelta(days=100))
+def load_green_model():
+  green_model=joblib.load("models/green_model.pkl")
+  
+  return green_model
+
+## get the  performance of both models:
+@task
+def blue_model_eval(X_test,y_test):
+  blue_metrics=model_metrics(y_test,X_test,blue_model)
+
+  return blue_metrics
+
+## get the  performance of both models:
+@task
+def green_model_eval():
+  green_metrics=model_metrics(y_test,X_test,green_model)
+  
+  return green_metrics
+
+## save the metrics of both models as individual pickle files:
+@task
+def save_model_metrics(blue_metrics,green_metrics):
+  joblib.dump(log_metrics,"metrics/blue_metrics.pkl")
+  joblib.dump(dt_metrics,"metrics/green_metrics.pkl")
