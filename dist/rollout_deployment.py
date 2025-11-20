be0d8c15bcc7042a208cdfb81d1dd262c6c64ecd
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-20 06:49:43 +0200

    imported logistic reg and dt from sklearn

--- dist/rollout_deployment.py
+++ dist/rollout_deployment.py
@@ -3,10 +3,11 @@ try:
   from fastapi import FastAPI,HTTPException
   from pydantic import BaseModel
   from schemas import LiveModel,PredictionInput
+  from sklearn.tree import DecisionTreeClassifier
+  from sklearn.linear_model import LogisticRegression
   
 except ImportError:
   raise ImportError("modules not found")
-
 
 
 
