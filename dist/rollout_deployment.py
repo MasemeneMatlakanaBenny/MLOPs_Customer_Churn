d979be029512c05ffa68dbca69829622a5e1e79f
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-20 06:48:33 +0200

    Import LiveModel and PredictionInput from schemas

--- dist/rollout_deployment.py
+++ dist/rollout_deployment.py
@@ -2,9 +2,11 @@ from typing import Optional
 try:
   from fastapi import FastAPI,HTTPException
   from pydantic import BaseModel
-
+  from schemas import LiveModel,PredictionInput
+  
 except ImportError:
   raise ImportError("modules not found")
+
 
 
 
