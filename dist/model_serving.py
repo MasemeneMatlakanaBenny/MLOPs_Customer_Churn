a97d9ca6f11feea542e9a3b1acea2dfd9f3533b1
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-20 06:42:38 +0200

    Load blue and green models from MLflow

--- dist/model_serving.py
+++ dist/model_serving.py
@@ -16,3 +16,6 @@ stage="production"
 
 blue_model_uri=f"models:/{blue_model_name}/{stage}"
 green_model_uri=f"models:/{green_model_name}/{stage}"
+
+blue_model=mlflow.pyfunc.load(model_uri=blue_model_uri)
+green_model=mlflow.pyfunc.load(model_uri=green_model_uri)
