47d064f592df1b56b5d492d3061ddfbece21df32
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-20 06:40:13 +0200

    Initialize MLflow setup in model_serving.py
    Import MLflow functions and set up experiment and model names.

--- dist/model_serving.py
+++ dist/model_serving.py
@@ -1,1 +1,12 @@
+import mlflow.sklearn
+from src.configurations_mlflow import set_mlflow_exp,set_mlflow_host,load_blue_model_name,load_green_model_name
+
+
+## set the mlflow host and experiment within the workflow:
+set_mlflow_host()
+set_mlflow_exp()
+
+## get the model names:
+blue_model_name=load_blue_model_name()
+green_model_name=load_green_model_name()
 
